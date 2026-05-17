f06c8d836e48432669b6d383b09aebff18e2568d
Syed <5y3d@example.com>
2026-05-17 19:51:58 +0100

    added function to get active series counts and label counts

--- smart_metrics/src/endpointScraper.ts
+++ smart_metrics/src/endpointScraper.ts
@@ -14,8 +14,10 @@ interface HistoricalAPIResponse {
 }
 
 const getAllHistoricalMetricNames = async () => {
+  //db will only have max 1 month's worth of data
   const endDate = new Date();
   const startDate = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
+  //need iso strings for vmselect url parameters
   const endIso = endDate.toISOString();
   const startIso = startDate.toISOString();
 
@@ -23,3 +25,38 @@ const getAllHistoricalMetricNames = async () => {
   return res.data.data;
 }
 
+interface ActiveMetrics {
+  status: string;
+  isPartial: boolean;
+  data: MetricsData;
+}
+
+interface MetricsData {
+  totalSeries: number;
+  totalLabelValuePairs: number;
+  seriesCountByMetricName: MetricCount[];
+  seriesCountByLabelName: LabelCount[];
+  seriesCountByFocusLabelValue: LabelValuePairCount[];
+  seriesCountByLabelValuePair: LabelValuePairCount[];
+  labelValueCountByLabelName: LabelCount[];
+}
+
+interface LabelCount {
+  name: string;
+  value: number;
+}
+
+interface MetricCount extends LabelCount {
+  requestsCount: number;
+  lastRequestTimestamp: number;
+}
+
+interface LabelValuePairCount extends LabelCount {}
+
+// we are interested in seriesCountByMetricName and labelValueCountByLabelName
+// use like this:
+// const { seriesCountByMetricName, labelValueCountByLabelName } = await getCurrentActiveMetrics();
+const getCurrentActiveMetrics = async () => {
+  const res = await axios.get<ActiveMetrics>(`${vmSelectEndpoint}//status/tsdb?topN=100`);
+  return res.data.data;
+}
