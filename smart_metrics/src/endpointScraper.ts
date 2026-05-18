8018d19031d665b88725571ba0a45e62f57e8d89
Syed <5y3d@example.com>
2026-05-18 17:39:47 +0100

    deleted historical api endpoint scraper as no longer needed

--- smart_metrics/src/endpointScraper.ts
+++ smart_metrics/src/endpointScraper.ts
@@ -7,27 +7,6 @@ dotenv.config();
 //read about tenant id
 const vmSelectEndpoint = process.env.VMSELECT_ENDPOINT || "http://localhost:8481/select/0/prometheus/api/v1"
 
-//this is in case victoria metrics was taken offline and brought online again
-//because the series won't be in memory so we have to do some manual api
-//scraping to get the data we need
-interface HistoricalAPIResponse {
-  status: string;
-  isPartial: boolean;
-  data: string[];
-}
-
-const getAllHistoricalMetricNames = async () => {
-  //db will only have max 1 month's worth of data
-  const endDate = new Date();
-  const startDate = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000)
-  //need iso strings for vmselect url parameters
-  const endIso = endDate.toISOString();
-  const startIso = startDate.toISOString();
-
-  const res = await axios.get<HistoricalAPIResponse>(`${vmSelectEndpoint}/label/__name__/values?start=${startIso}&end=${endIso}`);
-  return res.data.data;
-}
-
 interface BaseAPIResponse {
   status: string;
   isPartial: boolean;
